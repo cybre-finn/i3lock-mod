ae1fd0582572e59951b72fd8487f8930599a16e5
Ikarulus <github.ikarulus@zer.one>
2017-02-25 16:17:39 +0100

    add thinklight

--- lock_script.sh
+++ lock_script.sh
@@ -6,6 +6,7 @@ case "$1" in
     ;;
   *)
     mplayer sound.mp3 &
+    echo on > /proc/acpi/ibm/light
     feh -F "$(dirname "$0")"/failed.sh \
     & p="$!"; sleep 1.2; kill "$p" \
     & streamer -f jpeg -o "SOMEONE_TRIED_TO_ACCESS_YOUR_SCREEN_$(date)".jpeg
